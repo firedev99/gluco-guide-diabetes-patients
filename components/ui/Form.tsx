51c087c91fb01e6e2e8593fc80551441234bf62a
Israt-Jahan-Sujana <isratsujana25@gmail.com>
2024-06-21 08:50:43 +0600

    button_update

--- components/ui/Form.tsx
+++ components/ui/Form.tsx
@@ -1,7 +1,7 @@
 export default function Form() {
   return (
     <div className="w-[510px] h-4/5 flex flex-col justify-between items-center bg-white rounded-lg">
-      <form className="mt-4">
+      <form className="m-7">
         <h2 className="pt-4 text-align: center text-1xl font-bold">
           Welcome,Walter!
         </h2>
@@ -17,19 +17,69 @@ export default function Form() {
           alt=""
           className="mt-5 w-full h-28 border-2 border-sky-300 border-dashed rounded-xl"
         />
-        <br />
-        <div>
-          <input type="text" placeholder="First Name" />
+
+        <div className="pt-6 grid grid-cols-2 gap-2">
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Last Name"
+          />
+        </div>
+        <div className="pt-3 grid grid-cols-3 gap-2">
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Age"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Gender"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Profession"
+          />
+        </div>
+        <div className="pt-3 grid grid-cols-2 gap-2">
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Contact Number"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="E-mail Address"
+          />
+        </div>
+        <div className="pt-3 grid grid-cols-3 gap-2">
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Blood Group"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Weight"
+          />
+          <input
+            className="p-4 text-sm w-auto h-10 border-2 border-sky-500 border-solid rounded-xl"
+            type="text"
+            placeholder="Height"
+          />
         </div>
       </form>
-
-      <input
-        className="border-style: dashed border-2 border-sky-500 "
-        type="image"
-        src=""
-        alt=""
-      />
+      <button className="ms-96 mr-12 mb-14 w-28 h-10 pb-2 pt-2 border-none bg-indigo-600 rounded-lg text-center text-white text-sm">
+        Next
+      </button>
     </div>
   );
 }
-//border-color: rgb(163 230 53) border-style: solid h-3/5 width: 20% background-color: rgb(14 165 233)

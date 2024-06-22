fa9f23e34a11c94d708efd6f3a983e5c9e8338be
Israt-Jahan-Sujana <isratsujana25@gmail.com>
2024-06-22 09:03:35 +0600

    Form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Form from "@/components/ui/Form";
-
 export default function LoginPage() {
   return (
     <div className="max-h-screen flex flex-col justify-center h-screen items-center bg-gradient-to-b from-indigo-200 via-purple-100 to-pink-50">

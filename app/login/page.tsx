50f7188ccc5ee8a62af560224b43f0b7a76aee85
Israt-Jahan-Sujana <isratsujana25@gmail.com>
2024-06-23 00:53:47 +0600

    Base patient information form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import Form from "@/components/ui/Form";
+//import Form from "@/components/ui/Form";
 export default function LoginPage() {
   return (
     <div className="max-h-screen flex flex-col justify-center h-screen items-center bg-gradient-to-b from-indigo-200 via-purple-100 to-pink-50">
-      <Form />
+      {/* <Form /> */}
     </div>
     //<div className="min-h-screen flex items-center justify-center">
     // <Form />
